33764bbb94c7b49a57e15a79cc518100a2647adf
Kevin Reece <kreece@salesforce.com>
2018-08-03 08:43:13 +0100

    Removed Visual Picker, Fixed Grid, Split Out for Flow and Created Notifier Component for Notifications

---
+++ force-app/main/default/aura/TrainDataSet/TrainDataSetController.js
@@ -0,0 +1,8 @@
+({
+	train : function(component, event, helper) {
+        helper.train(component, event, helper);		
+    },
+    init : function(component, event, helper) {
+        helper.init(component, event, helper);		
+    }
+})
